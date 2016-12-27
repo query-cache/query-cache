fb64e1a259463cd45d4abac1f18190730c21882a
Fabian Franz <github@fabian-franz.de>
2016-12-27 15:06:09 -0800

    Fix parameters.

--- src/QueryFactory.php
+++ src/QueryFactory.php
@@ -8,18 +8,18 @@ namespace QueryCache;
 class QueryFactory
 {
 
-    public static function create($query, $args, $config)
+    public static function create($query, $args, $options, $config)
     {
         $query_type = Query::queryType($query);
         if ($query_type == 'INSERT' || $query_type == 'UPDATE' || $query_type == 'DELETE') {
-            return new CUDQuery($query, $args, $config);
+            return new CUDQuery($query, $args, $options, $config);
         }
 
         if ($query_type == 'SELECT') {
-            return new CacheableQuery($query, $args, $config);
+            return new CacheableQuery($query, $args, $options, $config);
         }
 
         // Fallback to execute the original query.
-        return new Query($query, $args, $config);
+        return new Query($query, $args, $options, $config);
     }
 }

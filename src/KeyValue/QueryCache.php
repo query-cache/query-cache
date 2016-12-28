26c42ee94a61c9500f8fa90d3ec97443f7f001b8
Fabian Franz <github@fabian-franz.de>
2016-12-28 14:30:04 -0800

    Re-organize for new structure.

---
+++ src/KeyValue/QueryCache.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace QueryCache\KeyValue;
+
+use QueryCache\QueryCache as BaseQueryCache;
+
+/**
+ * A class for caching key-value like queries.
+ */
+class QueryCache extends BaseQueryCache
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function invalidateQueryCache($cacheable_query)
+    {
+        $key = $cacheable_query->getKVCacheKey();
+
+        if ($key !== false) {
+          $cache_pool = $this->cachePoolFactory->get($cacheable_query->getKVCacheConfiguration());
+          $cache_pool->deleteItem($key);
+        }
+
+        parent::invalidateQueryCache($cacheable_query);
+        return;
+    }
+}
